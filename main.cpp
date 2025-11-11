2027b8f2d3f10e9e31f753511a867806ad8302bf
mohamed16007159 <mody4012@gmail.com>
2025-11-11 09:53:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,41 +8,22 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
 
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
 int main() {
-   
     
+    Instructor instructor("Dr. Lina Khaled", 1001, "Computer Science", 5);
+
+   
+    Course course("CS101", "Introduction to Programming", 3, instructor);
+
+    Student student("Omar Nabil", 2202, 2, "Informatics");
+    course.addStudent(student);
+   
+    course.displayCourseInfo();
+
     return 0;
 }
+
+   
+    
+ 
