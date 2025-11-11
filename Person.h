aa58c8104c4fcabd2e4cb630cc25e610bd5351e8
mohamed16007159 <mody4012@gmail.com>
2025-11-11 09:54:37 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,20 +1,26 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
 
+public:
+    Person();
+    Person(string n, int i);
+    virtual ~Person();
 
+    void setName(string n);
+    void setId(int i);
+    string getName() const;
+    int getId() const;
 
-
-
-
-
-
-
-
-
+    virtual void display() const;
+};
 
 #endif
